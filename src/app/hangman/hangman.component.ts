ca8f3cc44c4c2900aa00c7b7ca1bcdf19520a8cb
mflanders06 <mflanders06@gmail.com>
2022-05-13 09:34:18 -0600

    Setup for rendering hangman

--- src/app/hangman/hangman.component.ts
+++ src/app/hangman/hangman.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Joke } from '../joke';
+import { Joke, JokeCategory } from '../joke';
 import { DataService } from '../data.service';
 
 @Component({
@@ -10,13 +10,31 @@ import { DataService } from '../data.service';
 export class HangmanComponent implements OnInit {
 
   constructor(private dataService: DataService) { }
-  hangman: Joke | null = null
+  hangman: Joke = {
+                    id: 1,
+                    category: JokeCategory.hangman,
+                    question: "Loading",
+                    answer: "Loading",
+                    lastUsed: new Date('2022-05-02 00:00:00')
+
+  }
+  answerArray: string[] = []
 
   ngOnInit(): void {
+    this.getHangman();
   }
 
   getHangman(){
-    this.dataService.getNextHangman().subscribe(hangman => this.hangman = hangman)
+    this.dataService.getNextHangman().subscribe(hangman => {
+      this.hangman = hangman
+      this.splitAnswer()
+    })
   }
+
+  splitAnswer(){
+    this.answerArray = this.hangman?.answer.split("");
+  }
+
+  
 
 }
